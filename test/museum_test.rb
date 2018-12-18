79fbf7e9ccde03e8a18f77d66c4daebfdb92a6b5
Tim Allen <tnelsallen3@gmail.com>
2018-12-18 09:27:03 -0700

    Adds test it can recommend exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -35,4 +35,19 @@ class MuseumTest < Minitest::Test
 
     assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
+
+  def test_it_can_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+
+    assert_equal [dead_sea_scrolls, gems_and_minerals], dmns.recommend_exhibits(bob)
+    assert_equal [imax], dmns.recommend_exhibits(sally)
+  end
 end
