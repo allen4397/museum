b2821ea2ff2493efebd414a317da6b22592bd2a5
Tim Allen <tnelsallen3@gmail.com>
2018-12-18 09:12:14 -0700

    Adds spending money and attributes testr

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -9,4 +9,11 @@ class PatronTest < Minitest::Test
 
     assert_instance_of Patron, bob
   end
+
+  def test_it_has_attributes
+    bob = Patron.new("Bob", 20)
+
+    assert_equal "Bob", bob.name
+    assert_equal 20, bob.spending_money
+  end
 end
