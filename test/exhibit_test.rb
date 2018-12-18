81f29d10e624dfd2d3ff098129e4280947457f90
Tim Allen <tnelsallen3@gmail.com>
2018-12-18 09:07:48 -0700

    Adds test it has attributes and name and cost attribute to class

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -4,7 +4,15 @@ require './lib/exhibit'
 
 class ExhibitTest < Minitest::Test
   def test_it_exists
-    exhibit = Exhibit.new
+    exhibit = Exhibit.new("Gems and Minerals", 0)
+
     assert_instance_of Exhibit, exhibit
+  end
+
+  def test_it_has_attributes
+    exhibit = Exhibit.new("Gems and Minerals", 0)
+
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
   end
 end
