de959cb98c3205f3c5452984b16ef640e5627c39
Tim Allen <tnelsallen3@gmail.com>
2018-12-18 09:57:26 -0700

    Adds test it tracks patrons by exhibit interest

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -70,4 +70,25 @@ class MuseumTest < Minitest::Test
 
     assert_equal [bob, sally], dmns.patrons
   end
+
+  def test_it_tracks_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("Dead Sea Scrolls")
+
+    assert_equal ({
+                    gems_and_minerals => [bob],
+                    dead_sea_scrolls => [bob, sally],
+                    imax => []
+      }), dmns.patrons_by_exhibit_interest
+  end
 end
